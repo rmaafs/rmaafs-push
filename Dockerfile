af5fae8935119dbb6f6a2c5c7ca9224b5083d18a
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2023-11-22 22:03:34 -0600

    Init project

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Use a lightweight Nginx image as the base
+FROM nginx:alpine
+
+# Copy your HTML file and any other necessary static resources
+COPY index.html /usr/share/nginx/html/
+COPY firebase-messaging-sw.js /usr/share/nginx/html/
+# If you have other files (images, styles, scripts, etc.), copy them as well:
+# COPY static /usr/share/nginx/html/static
+
+# Expose port 80 to make the service accessible from outside
+EXPOSE 80
+
+# Command to start the Nginx server in the foreground
+CMD ["nginx", "-g", "daemon off;"]
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts("https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.6.2/firebase-messaging.js");
+// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
+importScripts("https://www.gstatic.com/firebasejs/8.6.2/firebase-messaging.js");
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notification = JSON.parse(payload.data.notification);
+  const notificationTitle = notification.title;
+  const notificationOptions = {
+    body: notification.body,
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
